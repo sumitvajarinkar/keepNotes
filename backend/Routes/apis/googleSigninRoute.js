70d18ce0cc09fcf6fdfafe9b8779890202d69484
Jaydeep <jaydeepshelake10@gmail.com>
2021-06-25 20:52:25 +0530

    completed google  auth

---
+++ backend/Routes/apis/googleSigninRoute.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken')
+const loginRouter= express.Router();
+const {OAuth2Client}= require('google-auth-library');
+
+const client = new OAuth2Client('740792960585-2d4uasquu6ddemf21i5jejkbp5o3grvk.apps.googleusercontent.com')
+//POST googlelogin
+loginRouter.post('/',async(req,res)=>{
+console.log(req.body.tokenId);
+const {tokenId}= req.body;
+const response=await client.verifyIdToken({idToken:tokenId,audience:'740792960585-2d4uasquu6ddemf21i5jejkbp5o3grvk.apps.googleusercontent.com'})
+console.log(response.payload)
+const {email_verified,email,name,picture} =response.payload;
+const secret='secret'
+if(email_verified){
+  User.findOne({email}).exec((err,user)=>{
+      if(err){
+         return res.status(400).json({
+              error:'Something went wrong'
+          })
+      }
+      else{
+          if(user){ //if user is alredy existing in database
+            const token=jwt.sign({_id:user._id},secret,{expiresIn:'7d'})
+            const {_id,name,email,picture}=user;
+            res.json({
+                token,
+                user:{_id,name,email,picture}
+            })
+          }
+          else{
+              let newUser=new User({name,email,picture})
+                  newUser.save((err,data)=>{
+                      if(err){
+                          return res.status(400).json({
+                              error:'Something went worng'
+                          })
+                      }
+                    const token=jwt.sign({_id:data._id},secret,{expiresIn:'7d'})
+                    const {_id,name,email,picture}=newUser;
+                    res.json({
+                        token,
+                        user:{_id,name,email,picture}
+                    })
+                 });
+          }
+      }
+  })
+}
+});
+
+module.exports=loginRouter;
---
+++ frontend/src/actions/index.js
@@ -0,0 +1,18 @@
+import {LOG_OUT, SIGN_IN, UPDATE_USER} from './types';
+import axios from 'axios';
+export const loginWithGoogle=(res)=>async dispatch=>{
+    const {data}=await axios.post('http://localhost:5000/googlelogin',{tokenId:res.tokenId})
+    console.log('Google login success',data)
+    localStorage.setItem('profile',JSON.stringify({...data}))
+    dispatch({type:SIGN_IN,payload:data})
+    dispatch(updateUser());
+}
+
+export const updateUser=()=>{
+  return{type:UPDATE_USER,payload:JSON.parse(localStorage.getItem('profile'))}
+}
+
+export const logout=()=>{
+  localStorage.clear();
+  return{type:LOG_OUT,payload:{}}
+}
